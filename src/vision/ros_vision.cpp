387933af33d042201e2d347c9f412b733171e42b
rvansoelen <vansoelen.1@osu.edu>
2016-02-22 21:39:01 -0500

    Test from new branch

--- src/vision/ros_vision.cpp
+++ src/vision/ros_vision.cpp
@@ -11,7 +11,7 @@ ros::Subscriber becn_req;
 //handler for beacon request
 void BeaconRequest_hand(const robot::BeaconRequest::ConstPtr& msg) {
 
-	//look for the beacon in the requested area
+	//look for the beacon in the requested area.
 	beacon_loc loc = beacon_main(msg->angle_min, msg->angle_max);
 	/*loc.angle_from_robot = 10;
 	loc.angle_from_beacon = 15;
