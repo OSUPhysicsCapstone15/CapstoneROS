4fafa23a1dc16deeda27874b61e8a2fe3c199c7f
buckeye <OSUPhysicsCapstoneMotion@gmail.com>
2016-10-21 13:03:44 -0400

    beacon_test added

---
+++ src/vision/beacon_test.cpp
@@ -0,0 +1,134 @@
+#include "beacon.h"
+#include "functions.h"
+
+void beacon_main(beacon_loc &b_loc)
+{
+    createWindows();
+
+    //Set up blob detection parameters
+    SimpleBlobDetector::Params params = setupBeaconBlobParams();
+    /* commented out by Kaeli for testing purposes */
+    //capture the video from web cam
+    VideoCapture cap(0);
+
+    //if not success, exit program
+    if (!cap.isOpened())
+    {
+        cout << "Cannot open the web cam" << endl;
+        return;    
+        }
+    
+    
+
+    Mat imgOriginal1 = getPic(cap); // imread("testPics2/imgOriginal1.jpg", CV_LOAD_IMAGE_COLOR);
+    Mat imgOriginal2 = getPic(cap); //imread("testPics2/imgOriginal2.jpg", CV_LOAD_IMAGE_COLOR); //getPic(cap);
+    Mat imgOriginal3 = getPic(cap); //imread("testPics2/imgOriginal3.jpg", CV_LOAD_IMAGE_COLOR); //getPic(cap);
+    Mat imgOriginal4 = getPic(cap); //imread("testPics2/imgOriginal4.jpg", CV_LOAD_IMAGE_COLOR); //getPic(cap);
+
+    if(imgOriginal1.empty() || imgOriginal2.empty() ||imgOriginal3.empty() ||imgOriginal4.empty())
+    {
+        cout << "can not open " << endl;
+        return;
+    }
+
+    //initialize mats
+    Mat diff_and,diff1,diff2,grayDiff,binDiff;
+
+    //diff1 = difference between img1 and img2
+    absdiff(imgOriginal1,imgOriginal2,diff1);
+
+    //diff2 = difference between img3 and img4
+    absdiff(imgOriginal3,imgOriginal4,diff2);
+
+    //logical AND diff1 and diff2 to eliminate people behind beacon
+    bitwise_and(diff1,diff2,diff_and);
+
+    //convert to grayscale
+    cvtColor(diff_and, grayDiff, COLOR_BGR2GRAY); //Convert the captured
+
+    //try to fill keypoints with exactly four points
+    double thresh = 150.0;
+    vector<KeyPoint> keypoints(0);
+
+    threshold(grayDiff, binDiff, thresh, 255, THRESH_BINARY);
+    dilate(binDiff, binDiff, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+
+    Ptr<SimpleBlobDetector> blobDetect = SimpleBlobDetector::create(params);
+    blobDetect->detect(binDiff, keypoints);
+
+    cout << "Keypoints size: " << keypoints.size() << endl;
+
+    while(keypoints.size() > 4 && thresh <= 255)
+    {
+        thresh += 5;
+        threshDilateDetect(grayDiff, binDiff, thresh, params, keypoints);
+    }
+
+    cout << "Keypoints size: " << keypoints.size() << endl;
+
+    while(keypoints.size() < 4 && thresh >= 50)
+    {
+        thresh -= 5;
+        threshDilateDetect(grayDiff, binDiff, thresh, params, keypoints);
+    }
+
+    if(thresh < 50)
+    {
+	cout << "Failed to find beacon" << endl;
+        b_loc.beacon_not_found = 1;
+        return;
+    }
+
+    cout << "Keypoints size: " << keypoints.size() << endl;
+
+    while(keypoints.size() > 4 && thresh <= 255)
+    {
+        thresh += .25;
+        threshDilateDetect(grayDiff, binDiff, thresh, params, keypoints);
+    }
+
+    cout << "Thresh: " << thresh << endl;
+    cout << "Keypoints size: " << keypoints.size() << endl;
+
+    //draw important points for output
+    Mat out = drawAndCircleKeypoints(keypoints, binDiff);
+
+    if(keypoints.size() == 4)
+    {
+        b_loc.beacon_not_found = 0;
+        b_loc.only_bottom = 0;
+
+        Point cent = findCenterPoint(keypoints);
+
+	beaconLocation(keypoints, &b_loc);
+
+    // getBeaconOrientation(keypoints, &b_loc);
+    // printDistanceFromLights(keypoints, &b_loc);
+    robot_angle(&b_loc, binDiff, cent.x);
+
+    circle(out, cent, 5, CV_RGB(0,100,0), -1, 8);
+    }
+    else if(keypoints.size() == 1)
+    {
+        b_loc.beacon_not_found = 0;
+        b_loc.only_bottom = 1;
+
+        robot_angle(&b_loc, binDiff, keypoints.front().pt.x);
+    }
+    else
+    {
+        b_loc.beacon_not_found = 1;
+    }
+    imwrite("testPics/imgOriginal1.jpg", imgOriginal1);
+    imwrite("testPics/imgOriginal2.jpg", imgOriginal2);
+    imwrite("testPics/imgOriginal3.jpg", imgOriginal3);
+    imwrite("testPics/imgOriginal4.jpg", imgOriginal4);
+    imwrite("testPics/diff1.jpg", diff1);
+    imwrite("testPics/diff2.jpg", diff2);
+    imwrite("testPics/out.jpg", out);
+
+cout << "HERE!" << endl;
+    //output windows to view
+    showWindows(imgOriginal1, imgOriginal2, imgOriginal3, imgOriginal4, diff1, diff2, out);
+}
+
