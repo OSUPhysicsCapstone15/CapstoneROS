cc6b90ded530215361155b81b3b7bfc678c9e610
buckeye <OSUPhysicsCapstoneMotion@gmail.com>
2016-10-21 14:12:13 -0400

    more test files added

---
+++ src/vision/main.cpp
@@ -0,0 +1,22 @@
+#include "blob.h"
+#include "beacon.h"
+#include "ros/ros.h"
+
+int main(){
+/*
+  to test beacon.cpp */
+
+	beacon_loc bloc;
+	bloc.x=0;
+	bloc.y=9;
+	bloc.angle_from_robot=0;
+	beacon_main(bloc);
+
+/* to test blob.cpp
+
+	sample_loc sloc;
+	blob_main(sloc);
+*/
+
+return 0;
+}
